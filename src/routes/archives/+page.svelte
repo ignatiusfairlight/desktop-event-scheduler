da2fb6bbb38f5a08ed98c4843a841b2e03402e04
Ignatius Fairlight <lawaierang@gmail.com>
2026-03-29 19:23:35 +0800

    feat: - Archives now only show approved events that has occurred

--- src/routes/archives/+page.svelte
+++ src/routes/archives/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import DataTable from "$lib/components/data-table.svelte";
-  import { columns, type Event } from "./columns";
+  import { columns } from "./columns";
+  import { type Event } from "$lib/tableUtils";
   import { onMount } from "svelte";
   import { invoke } from "@tauri-apps/api/core";
 
@@ -15,10 +16,16 @@
         console.error(e);
       });
   });
+
+  let archivedData = $derived(
+    data.filter(
+      (event) => event.is_approved === 1 && new Date(event.end) < new Date(),
+    ),
+  );
 </script>
 
 <h2 class="mt-5 mb-4 ml-10 text-4xl font-semibold">Archives</h2>
 
 <div class="flex flex-col gap-10 p-4 xl:flex-row xl:p-10">
-  <DataTable {data} {columns} />
+  <DataTable data={archivedData} {columns} />
 </div>
