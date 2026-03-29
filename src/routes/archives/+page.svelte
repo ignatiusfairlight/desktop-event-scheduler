b9846114e5542e0457f29e14fcd199901c089a0e
Ignatius Fairlight <lawaierang@gmail.com>
2026-03-29 16:03:48 +0800

    feat: - added data tables to Archives

--- src/routes/archives/+page.svelte
+++ src/routes/archives/+page.svelte
@@ -1,16 +1,24 @@
 <script lang="ts">
+  import DataTable from "./data-table/data-table.svelte";
+  import { columns, type Event } from "./data-table/columns";
+  import { onMount } from "svelte";
+  import { invoke } from "@tauri-apps/api/core";
 
+  let data = $state<Event[]>([]);
+
+  onMount(async () => {
+    invoke<Event[]>("get_events")
+      .then((result) => {
+        data = result;
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  });
 </script>
 
 <h2 class="mt-5 mb-4 ml-10 text-4xl font-semibold">Archives</h2>
 
 <div class="flex flex-col gap-10 p-4 xl:flex-row xl:p-10">
-  <div class="grow xl:basis-2/3 xl:pr-4">
-
-  </div>
-  <div class="min-w-0 grow xl:basis-1/3">
-    <div class="w-full overflow-hidden">
-
-    </div>
-  </div>
+  <DataTable {data} {columns} />
 </div>
