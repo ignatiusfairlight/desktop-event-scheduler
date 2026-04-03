630fa44170004883c3c9822f27d2991dfc204112
Ignatius Fairlight <lawaierang@gmail.com>
2026-04-04 05:39:43 +0800

    feat: - added Input to filter by event title - added selection to filter by month

---
+++ src/lib/components/event-filter.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { Input } from "$lib/components/ui/input/index.js";
+    import * as Select from "$lib/components/ui/select/index.js";
+
+    let {
+        filterTitle = $bindable(),
+        filterStartMonth = $bindable(),
+        filterEndMonth = $bindable(),
+        filterStatus = $bindable(),
+    } = $props();
+
+    const months = [
+        { value: "", label: "Select a month"},
+        { value: "0", label: "January" },
+        { value: "1", label: "February" },
+        { value: "2", label: "March" },
+        { value: "3", label: "April" },
+        { value: "4", label: "May" },
+        { value: "5", label: "June" },
+        { value: "6", label: "July" },
+        { value: "7", label: "August" },
+        { value: "8", label: "September" },
+        { value: "9", label: "October" },
+        { value: "10", label: "November" },
+        { value: "11", label: "December" },
+    ];
+
+    const startSelectTrigger = $derived(
+        months.find((f) => f.value === filterStartMonth)?.label ??
+            "Select a month",
+    );
+
+    const startEndTrigger = $derived(
+        months.find((f) => f.value === filterEndMonth)?.label ??
+            "Select a month",
+    );
+</script>
+
+<div>
+    <Input
+        bind:value={filterTitle}
+        placeholder="Enter event title"
+        class="max-w-xs"
+    />
+</div>
+
+<div>
+    <Select.Root type="single" bind:value={filterStartMonth}>
+        <Select.Trigger class="w-[180px]">{startSelectTrigger}</Select.Trigger>
+        <Select.Content>
+            {#each months as month}
+                <Select.Item value={month.value}>{month.label}</Select.Item>
+            {/each}
+        </Select.Content>
+    </Select.Root>
+    <Select.Root type="single" bind:value={filterEndMonth}>
+        <Select.Trigger class="w-[180px]">{startEndTrigger}</Select.Trigger>
+        <Select.Content>
+            {#each months as month}
+                <Select.Item value={month.value}>{month.label}</Select.Item>
+            {/each}
+        </Select.Content>
+    </Select.Root>
+</div>
+
+<div></div>
--- src/routes/event-details/+page.svelte
+++ src/routes/event-details/+page.svelte
@@ -4,8 +4,16 @@
     import { type Event } from "$lib/tableUtils";
     import { onMount } from "svelte";
     import { invoke } from "@tauri-apps/api/core";
+    import EventFilter from "$lib/components/event-filter.svelte";
 
     let data = $state<Event[]>([]);
+
+    let filters = $state({
+        title: "",
+        startMonth: "",
+        endMonth: "",
+        status: [] as number[],
+    });
 
     onMount(async () => {
         invoke<Event[]>("get_events")
@@ -24,10 +32,36 @@
                 (a, b) => new Date(a.end).getTime() - new Date(b.end).getTime(),
             ),
     );
+
+    let filteredResults = $derived(
+        ongoingData.filter(
+            (event) =>
+                (filters.title === "" ||
+                    event.title
+                        .toLowerCase()
+                        .includes(filters.title.toLowerCase())) &&
+                (filters.startMonth === "" ||
+                    new Date(event.start).getMonth() >=
+                        parseInt(filters.startMonth)) &&
+                (filters.endMonth === "" ||
+                    new Date(event.end).getMonth() <=
+                        parseInt(filters.endMonth)) &&
+                (filters.status.length === 0 ||
+                    filters.status.includes(event.is_approved)),
+        ),
+    );
 </script>
 
 <h2 class="mt-5 mb-4 ml-10 text-4xl font-semibold">Event Details</h2>
 
+<div>
+    <EventFilter
+        bind:filterTitle={filters.title}
+        bind:filterStartMonth={filters.startMonth}
+        bind:filterEndMonth={filters.endMonth}
+        bind:filterStatus={filters.status}
+    />
+</div>
 <div class="flex flex-col gap-10 p-4 xl:flex-row xl:p-10">
-    <DataTable data={ongoingData} {columns} showPagination={true} />
+    <DataTable data={filteredResults} {columns} showPagination={true} />
 </div>
