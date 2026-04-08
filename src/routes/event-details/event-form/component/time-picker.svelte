e1a5036ad26db382ab4c4de9db9c9237db8bf1c4
Ignatius Fairlight <lawaierang@gmail.com>
2026-04-09 05:53:55 +0800

    changes: - removed step="1"

--- src/routes/event-details/event-form/component/time-picker.svelte
+++ src/routes/event-details/event-form/component/time-picker.svelte
@@ -10,14 +10,13 @@
 
 <!--TODO-->
 <!-- Create a logic to prevent the user from picking time occurs before the start -->
+<!-- Change this shitty time picker into something easier to use -->
 
 <div>
     <div class="flex flex-col gap-3">
-        <Label for="{id}-time" class="px-1">Time</Label>
         <Input
             type="time"
             id="{id}-time"
-            step="1"
             bind:value={time}
             class="
                 bg-background appearance-none 
