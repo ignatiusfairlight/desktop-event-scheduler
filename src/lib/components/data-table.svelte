b3298a05802cb57cdd02aea046e5d60d3c24ea1b
Ignatius Fairlight <lawaierang@gmail.com>
2026-04-14 04:58:03 +0800

    changes: - ui polish for the data-table

--- src/lib/components/data-table.svelte
+++ src/lib/components/data-table.svelte
@@ -50,58 +50,60 @@
   });
 </script>
 
-<div class="rounded-md border">
-  <Table.Root>
-    <Table.Header>
-      {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-        <Table.Row>
-          {#each headerGroup.headers as header (header.id)}
-            <Table.Head colspan={header.colSpan}>
-              {#if !header.isPlaceholder}
+<div class="w-full">
+  <div class="rounded-md border">
+    <Table.Root>
+      <Table.Header>
+        {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+          <Table.Row>
+            {#each headerGroup.headers as header (header.id)}
+              <Table.Head colspan={header.colSpan}>
+                {#if !header.isPlaceholder}
+                  <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                  />
+                {/if}
+              </Table.Head>
+            {/each}
+          </Table.Row>
+        {/each}
+      </Table.Header>
+      <Table.Body>
+        {#each table.getRowModel().rows as row (row.id)}
+          <Table.Row data-state={row.getIsSelected() && "selected"}>
+            {#each row.getVisibleCells() as cell (cell.id)}
+              <Table.Cell>
                 <FlexRender
-                  content={header.column.columnDef.header}
-                  context={header.getContext()}
+                  content={cell.column.columnDef.cell}
+                  context={cell.getContext()}
                 />
-              {/if}
-            </Table.Head>
-          {/each}
-        </Table.Row>
-      {/each}
-    </Table.Header>
-    <Table.Body>
-      {#each table.getRowModel().rows as row (row.id)}
-        <Table.Row data-state={row.getIsSelected() && "selected"}>
-          {#each row.getVisibleCells() as cell (cell.id)}
-            <Table.Cell>
-              <FlexRender
-                content={cell.column.columnDef.cell}
-                context={cell.getContext()}
-              />
+              </Table.Cell>
+            {/each}
+          </Table.Row>
+        {:else}
+          <Table.Row>
+            <Table.Cell colspan={columns.length} class="h-24 text-center">
+              No results.
             </Table.Cell>
-          {/each}
-        </Table.Row>
-      {:else}
-        <Table.Row>
-          <Table.Cell colspan={columns.length} class="h-24 text-center">
-            No results.
-          </Table.Cell>
-        </Table.Row>
-      {/each}
-    </Table.Body>
-  </Table.Root>
-</div>
-
-{#if showPagination}
-  <div>
-    <ButtonGroup.Root>
-      <Button
-        disabled={!table.getCanPreviousPage()}
-        onclick={() => table.previousPage()}>Previous</Button
-      >
-      <Button
-        disabled={!table.getCanNextPage()}
-        onclick={() => table.nextPage()}>Next</Button
-      >
-    </ButtonGroup.Root>
+          </Table.Row>
+        {/each}
+      </Table.Body>
+    </Table.Root>
   </div>
-{/if}
+
+  {#if showPagination}
+    <div class="flex justify-center mt-4">
+      <ButtonGroup.Root>
+        <Button
+          disabled={!table.getCanPreviousPage()}
+          onclick={() => table.previousPage()}>Previous</Button
+        >
+        <Button
+          disabled={!table.getCanNextPage()}
+          onclick={() => table.nextPage()}>Next</Button
+        >
+      </ButtonGroup.Root>
+    </div>
+  {/if}
+</div>
